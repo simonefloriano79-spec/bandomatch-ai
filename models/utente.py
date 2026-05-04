b482448ab25f130f86495de6f43875d14ad208ec
simonefloriano79-spec <simone.floriano79@gmail.com>
2026-05-04 10:22:00 +0200

    Update utente.py

--- models/utente.py
+++ models/utente.py
@@ -1,5 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
+from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from typing import Dict, Any, Optional
